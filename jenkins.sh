1e42ad53f1b64f68fe2ea1b89ce9221ea4b70d53
ReadMeVishal <vishalashokedu@gmail.com>
2021-12-28 16:52:24 +0530

    jenkins automate

---
+++ jenkins.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt update -y
+
+sudo apt install default-jre -y
+
+java -version
+
+sudo apt update -y
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt update -y
+
+sudo add-apt-repository universe -y
+
+sudo apt-get install jenkins
+
+sudo service jenkins start
+
+cat /var/lib/jenkins/secrets/initialAdminPassword
+
+#sudo service jenkins status
+
+
+#34.232.26.121
+
