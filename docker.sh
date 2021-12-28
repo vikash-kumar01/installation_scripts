a5344ac23334a1c6817c460f17f007a7152c26f3
ReadMeVishal <vishalashokedu@gmail.com>
2021-12-28 16:12:26 +0530

    docker shell

---
+++ docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update -y
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+
+sudo apt update -y
+
+apt-cache policy docker-ce -y
+
+sudo apt install docker-ce -y
+
+sudo systemctl status docker
+
+sudo chmod 777 /var/run/docker.sock
