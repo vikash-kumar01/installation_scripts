9d6edb25372249f86f0d9055952deeb1bdfd0879
ReadMeVishal <vishalashokedu@gmail.com>
2021-12-30 14:52:34 +0530

    minikube install

---
+++ minikube.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sudo su
+
+echo "1st install docker"
+
+sudo apt update && apt -y install docker.io
+
+echo "install Kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+echo "Now install Minikube"
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube -y
+
+echo "now install conntrack"
+apt install conntrack -y
+
+echo "Now start minikube"
+minikube start --vm-driver=none -y
+
+echo "Directory works"
+sudo mv /root/.kube /root/.minikube $HOME
+sudo chown -R $USER $HOME/.kube $HOME/.minikube
