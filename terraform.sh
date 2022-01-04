90e4b8fa92519a7fbe93889cb7770db0b21820ff
Vikash Kumar <35370115+vikash-kumar01@users.noreply.github.com>
2022-01-04 14:26:14 +0530

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+sudo apt-get install unzip
+
+wget https://releases.hashicorp.com/terraform/1.0.7/terraform_1.0.7_linux_amd64.zip
+
+unzip terraform_1.0.7_linux_amd64.zip
+
+sudo mv terraform /usr/local/bin/
+
+terraform --version 
