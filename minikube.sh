e09bfb216dd76442432af49ced1366ab2f5e651a
Vikash Kumar <35370115+vikash-kumar01@users.noreply.github.com>
2022-07-19 13:59:01 +0530

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -4,22 +4,26 @@ echo "1st install docker"
 
 sudo apt update && apt -y install docker.io
 
-echo "install Kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-kubectl version --client
+echo "Apply updates"
+sudo apt update -y 
+sudo apt upgrade -y
 
-echo "Now install Minikube"
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube 
+echo " Download Minikube Binary"
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo cp minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod +x /usr/local/bin/minikube
+minikube version
 
-echo "now install conntrack"
-apt install conntrack -y
 
-echo "Now start minikube"
-minikube start --vm-driver=none 
+echo "Install Kubectl utility"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version -o yaml
 
-echo "Directory works"
-sudo mv /root/.kube /root/.minikube $HOME
-sudo chown -R $USER $HOME/.kube $HOME/.minikube
+
+echo "Start the minikube"
+minikube start --driver=docker
+minikube status
+
+
