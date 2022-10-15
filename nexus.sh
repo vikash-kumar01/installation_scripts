068919d1b179188b9cff81d1ecacbdab98bd6054
Vikash Kumar <35370115+vikash-kumar01@users.noreply.github.com>
2022-10-15 09:16:05 +0530

    Create nexus.sh

---
+++ nexus.sh
@@ -0,0 +1,15 @@
+apt-get update -y
+
+echo "Install Java"
+apt-get install openjdk-8-jdk -y
+java -version
+
+echo "Install Nexus"
+useradd -M -d /opt/nexus -s /bin/bash -r nexus
+echo "nexus ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/nexus
+mkdir /opt/nexus
+wget https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.29.2-02-unix.tar.gz
+tar xzf nexus-3.29.2-02-unix.tar.gz -C /opt/nexus --strip-components=1
+chown -R nexus:nexus /opt/nexus
+
+nano /opt/nexus/bin/nexus.vmoptions
