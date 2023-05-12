f23457f22777ea1d32a7ef9cf191be43c28b6724
Vikash Kumar <vikashashoke@gmail.com>
2023-05-12 14:25:27 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -11,5 +11,5 @@ curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get update -y 
+sudo apt-get install jenkins -y
