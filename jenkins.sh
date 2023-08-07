af0bcb838bd1dcfdffb2d2d68fc04d4eb6bb31f7
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-08-07 11:04:21 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 
 sudo apt upgrade -y 
 
-sudo apt install openjdk-17-jre -y
+sudo apt install openjdk-11-jre -y
 
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
